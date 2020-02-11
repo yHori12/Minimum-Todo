f7e2f3e381ba792e7c5802c81832cffe3d119ee7
yHori12 <hori@yadokari.tv>
2020-02-12 00:18:26 +0900

    CreateTaskFragmentのuiを修正

--- app/src/main/java/com/y_hori/minimum_todo/ui/create/CreateTaskFragment.kt
+++ app/src/main/java/com/y_hori/minimum_todo/ui/create/CreateTaskFragment.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import com.y_hori.minimum_todo.R
+import com.y_hori.minimum_todo.databinding.FragmentCreateTaskBinding
+import com.y_hori.minimum_todo.utils.InjectorUtils
 
 class CreateTaskFragment : Fragment() {
 
@@ -15,19 +20,42 @@ class CreateTaskFragment : Fragment() {
             CreateTaskFragment()
     }
 
-    private lateinit var viewModel: CreateTaskViewModel
+    private lateinit var binding:FragmentCreateTaskBinding
+    private val createTaskViewModel: CreateTaskViewModel by viewModels {
+        InjectorUtils.provideCreateTaskViewModelFactory()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_create_task, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_create_task,container,false
+        )
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CreateTaskViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        binding.apply {
+            this.viewModel = createTaskViewModel
+            this.lifecycleOwner = viewLifecycleOwner
+            this.title.doAfterTextChanged { input ->
+                if (input.toString().isEmpty()) return@doAfterTextChanged
+                createTaskViewModel.updateTitle(input.toString())
+            }
+            description.doAfterTextChanged{ input ->
+                if (input.toString().isEmpty()) return@doAfterTextChanged
+                createTaskViewModel.updateDescription(input.toString())
+            }
+            this.buttonCreate.setOnClickListener { view ->
+                view.findNavController()
+                    .navigate(CreateTaskFragmentDirections.actionCreateTaskFragmentToMainFragment())
+            }
+
+        }
     }
 
 }
